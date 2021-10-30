5ee6bb9640ad05b168fd180241cbfde5ca045a37
Chema Segarra <chegarra@outlook.com>
2021-10-30 09:57:46 +0200

    Fase 1

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# Variable por cada tipo de billete
+b5, b10, b20, b50 = 0, 0, 0, 0
+b100, b200, b500 = 0, 0, 0
+
+total = 0
+
+menu_platos = list()
+precios_platos = list()
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
